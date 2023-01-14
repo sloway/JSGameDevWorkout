1b07dca599817629b84479b23eda4d4b7cd2850f
sloway <touchthesky80s@gmail.com>
2023-01-14 17:29:19 +0900

    project2 done

--- project2/script.js
+++ project2/script.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext("2d");
 const CAVNAS_WIDTH = (canvas.width = 800);
 const CANVAS_HEIGHT = (canvas.height = 700);
 let gameSpeed = 15;
+//let gameFrame = 0;
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = "layer-1.png";
@@ -19,18 +20,64 @@ backgroundLayer4.src = "layer-4.png";
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = "layer-5.png";
 
-let x = 0;
-let x2 = 2400;
+window.addEventListener("load", () => {
+  class Layer {
+    constructor(image, speedModifier) {
+      this.x = 0;
+      this.y = 0;
+      this.width = 2400;
+      this.height = 700;
+      this.image = image;
+      this.speedModifier = speedModifier;
+      this.speed = gameSpeed * this.speedModifier;
+    }
 
-function animate() {
-  ctx.clearRect(0, 0, CAVNAS_WIDTH, CANVAS_HEIGHT);
-  ctx.drawImage(backgroundLayer4, x, 0);
-  ctx.drawImage(backgroundLayer4, x2, 0);
-  if (x < -2400) x = 2400 + x2 - gameSpeed;
-  else x -= gameSpeed;
-  if (x2 < -2400) x2 = 2400 + x - gameSpeed;
-  else x2 -= gameSpeed;
-  requestAnimationFrame(animate);
-}
+    update() {
+      this.speed = gameSpeed * this.speedModifier;
+      if (this.x <= -this.width) {
+        this.x = 0;
+      }
+      this.x = Math.floor(this.x - this.speed);
+      //this.x =- gameFrame * this.speed & this.width;
+    }
 
-animate();
+    draw() {
+      ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+      ctx.drawImage(
+        this.image,
+        this.x + this.width,
+        this.y,
+        this.width,
+        this.height
+      );
+    }
+  }
+
+  const layer1 = new Layer(backgroundLayer1, 0.2);
+  const layer2 = new Layer(backgroundLayer2, 0.4);
+  const layer3 = new Layer(backgroundLayer3, 0.6);
+  const layer4 = new Layer(backgroundLayer4, 0.8);
+  const layer5 = new Layer(backgroundLayer5, 1);
+
+  const slider = document.getElementById("slider");
+  slider.value = gameSpeed;
+  const showGameSpeed = document.getElementById("showGameSpeed");
+  showGameSpeed.innerHTML = gameSpeed;
+
+  slider.addEventListener("change", (e) => {
+    gameSpeed = e.target.value;
+    showGameSpeed.innerHTML = gameSpeed;
+  });
+  const gameObjects = [layer1, layer2, layer3, layer4, layer5];
+  function animate() {
+    ctx.clearRect(0, 0, CAVNAS_WIDTH, CANVAS_HEIGHT);
+    gameObjects.forEach((object) => {
+      object.update();
+      object.draw();
+    });
+    //--gameFrame;
+    requestAnimationFrame(animate);
+  }
+
+  animate();
+});
