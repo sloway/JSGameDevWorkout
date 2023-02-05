9ba4db724d0987e733da8c05c8217d98c93528fc
sloway <touchthesky80s@gmail.com>
2023-02-05 10:28:07 +0900

    Add Player class

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import { Player } from "./player.js";
+
 window.addEventListener("load", () => {
   const canvas = document.getElementById("canvas1");
   const ctx = canvas.getContext("2d");
@@ -8,10 +10,26 @@ window.addEventListener("load", () => {
     constructor(width, height) {
       this.width = width;
       this.height = height;
+      this.player = new Player(this);
     }
 
-    update() {}
+    update() {
+      this.player.update();
+    }
 
-    draw() {}
+    draw(context) {
+      this.player.draw(context);
+    }
   }
+
+  const game = new Game(canvas.width, canvas.height);
+
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    game.update();
+    game.draw(ctx);
+    requestAnimationFrame(animate);
+  }
+
+  animate();
 });
