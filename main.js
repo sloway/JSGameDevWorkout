973662664c748a915ec0224106c8e5bee558d433
sloway <touchthesky80s@gmail.com>
2023-02-05 13:50:02 +0900

    Add stats.js to monitor performance

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 import { Player } from "./player.js";
+import Stats from "./Stats.js";
+
+var stats;
+function initPerformanceMonitor() {
+  stats = new Stats();
+  stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+  document.body.appendChild(stats.dom);
+}
 
 window.addEventListener("load", () => {
+  initPerformanceMonitor();
+
   const canvas = document.getElementById("canvas1");
   const ctx = canvas.getContext("2d");
   canvas.width = 500;
@@ -25,9 +35,13 @@ window.addEventListener("load", () => {
   const game = new Game(canvas.width, canvas.height);
 
   function animate() {
+    stats.begin();
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     game.update();
     game.draw(ctx);
+
+    stats.end();
     requestAnimationFrame(animate);
   }
 
