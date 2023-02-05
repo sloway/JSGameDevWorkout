64b73d493b2ee885db49449406111173317da575
sloway <touchthesky80s@gmail.com>
2023-02-05 20:39:11 +0900

    Added enemies assets and class

---
+++ enemies.js
@@ -0,0 +1,19 @@
+class Enemy {
+  constructor() {
+    this.frameX = 0;
+    this.frameY = 0;
+    this.fps = 20;
+    this.frameInterval = 1000 / this.fps;
+    this.frameTimer = 0;
+  }
+
+  update() {}
+
+  draw() {}
+}
+
+class FlyingEnemy extends Enemy {}
+
+class GroundEnemy extends Enemy {}
+
+class ClimbingEnemy extends Enemy {}
